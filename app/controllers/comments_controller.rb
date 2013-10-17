39f88050195c04e6b0e1a05224d5643ad450c629
Robert Kusmirek <robert@kusmirek.pl>
2013-10-17 09:13:22 -0700

    w widoku posta dodanie wyswietlania komentarzy oraz komentowania

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment])
+    @comment.save
+    redirect_to @post
+  end
 end
